b065c65fbfb66cdb9ed291bc62bc5f520cab8fc6
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-14 18:04:21 +0900

    feat: (Models/TimeseriesAlias.swift)

---
+++ FinanceStarter/Models/TimeseriesAlias.swift
@@ -0,0 +1,9 @@
+//
+//  TimeseriesAlias.swift
+//  FinanceStarter
+//
+//  Created by Dev Tech on 2025/09/12.
+//
+
+// Models/TimeseriesAlias.swift   // <-- [ADDED]
+typealias Timeseries = TimeseriesResponse
